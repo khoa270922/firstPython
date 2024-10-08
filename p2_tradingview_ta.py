98fb19fa8c0ac8bf89ebfe7b457933bc7e34607d
Kenny Jade <ntdkhoa@hotmail.com>
2024-10-08 07:56:38 +0700

    20241008_080000

--- p2_tradingview_ta.py
+++ p2_tradingview_ta.py
@@ -1,5 +1,5 @@
 from tradingview_ta import TradingView, TA_Handler, Interval, Exchange
-
+'''
 interval = ['1m', '5m', '15m', '30m', '1h', '2h', '4h', '1d', '1W', '1M']
 tesla = TA_Handler(
     symbol = 'ACB',
@@ -15,8 +15,9 @@ tesla = TA_Handler(
     exchange="NASDAQ",
     interval=Interval.INTERVAL_1_DAY
 )
-'''
+
 #print(tesla.get_analysis().indicators)
 print(tesla.get_analysis().summary)
 #print(tesla.get_analysis().oscillators)
 #print(tesla.get_analysis().moving_averages)
+#print(TradingView.search('TSLA', 'america'))
